6ee3c73038bac78ccf5b64aaef5ef4b05ee090b6
Alex Tea <alextea2@gmail.com>
2019-06-29 16:02:29 +0200

    small css changes

--- src/components/Card/CardList.js
+++ src/components/Card/CardList.js
@@ -4,7 +4,7 @@ import CardPreview from "./CardPreview";
 const CardList = (props) => {
   const { id, name, cards } = props.list;
   return <div className="flex flex-col p-6 mr-2 bg-gray-200">
-      <p className="font-bold uppercase text-gray-800 mb-4">{name}</p>
+      <p className="font-bold uppercase text-blue-700 mb-4">{name}</p>
       {
         cards.map( (card, index) => <CardPreview key={index} card={card}/>)
       }
--- src/components/Card/CardPreview.js
+++ src/components/Card/CardPreview.js
@@ -4,12 +4,12 @@ import LabelSmall from "../Label/LabelSmall";
 const CardPreview = ({ card }) => {
   
   return <div className="cursor-pointer bg-gray-100 w-64 mb-6 shadow p-4">
-    <div className="flex">
+    <div className="flex mb-3">
       {
         card.labels.map( (label, index) => <LabelSmall key={index} name={label.name} color={label.color} /> )
       }
     </div>
-    <p className="text-gray-800 font-semibold mt-3">{card.title}</p>
+    <p className="text-gray-700 font-semibold">{card.title}</p>
   </div>
 
 };
