6231db5145b7daa4d25aef3363bd214d35da81c5
Alex Tea <alextea2@gmail.com>
2019-07-01 14:01:55 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CardProvider from "./components/context/CardProvider";
 import CardListContainer from "./components/Card/CardListContainer";
 import {DragDropContext} from "react-beautiful-dnd";
+import CardContext from "./components/context/CardContext";
 
 function App() {
+  
+  const context = useContext(CardContext);
+  
+  const onDragEnd = result => {
+    const { destination, source, draggableId } = result;
+    if(!destination){
+      return;
+    }
+    if(destination.droppableId === source.droppableId && destination.index === source.index){
+      return;
+    }
+    const lists = context.lists;
+    console.log(context, lists, lists.forEach(el => console.log(el)));
+    debugger;
+  };
+  
   return (
     <div className="App">
       <header className="text-center font-bold text-3xl text-blue-600 py-4">Mini trello !</header>
-        <DragDropContext onDragEnd={() => {}}>
+        <DragDropContext onDragEnd={onDragEnd}>
           <CardProvider>
             <CardListContainer/>
           </CardProvider>
